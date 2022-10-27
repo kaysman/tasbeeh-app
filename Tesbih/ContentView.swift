9bacc53db9581b273c493f0273950fc408ac28e9
kaysman <ikays.holding.com>
2022-10-27 17:53:16 +0500

    Initial Commit

---
+++ Tesbih/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tesbih
+//
+//  Created by MacBook Pro on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tesbih/TesbihApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesbihApp.swift
+//  Tesbih
+//
+//  Created by MacBook Pro on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TesbihApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
